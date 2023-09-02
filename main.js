bf90ec4f572e3abc4f401805f0002d81f47467d3
Anna Borisenko <anik4366@mail.ru>
2023-09-02 10:26:26 +0700

    проверка не пустой ли инпут

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function clearInput() {
 
 //выполняемый код
 btnNode.addEventListener('click', function () {
-  const movieName = inputNode.value;
+  const movieName = inputNode.value.trim();//trim убирает пробелы при вводе названия фильма
   const url = `https://www.omdbapi.com/?s=${movieName}&apikey=${apiKey}`;
 
   if (movieName) {
@@ -29,45 +29,26 @@ btnNode.addEventListener('click', function () {
         searchResults = data.Search;
         movieListNode.innerHTML = '';
 
-        // if (data.Response === 'True') {
-        //   searchResults.forEach((movie) => {
-        //     const movieHTML = `
-        // <div class="card">
-        //         <img src="${movie.Poster}" alt="Cover" class="card__img" />
-        //         <h3 class="card__title">"${movie.Title}"</h3>
-        //         <p class="card__year">"${movie.Year}"</p>
-        //         <p class="card__rate">Рейтинг: "${movie.Type}"</p>
-        //       </div>
-        // `;
-
-        //     movieListNode.insertAdjacentHTML('beforeend', movieHTML);
-        //   });
-        //}
-
-          if (data.Response === 'False') {
-            alert = ('Error ocured')
-          }
-
-          else {
-          searchResults.forEach((movie) => {
-            const movieHTML = `
-        <div class="card">
-                <img src="${movie.Poster}" alt="Cover" class="card__img" />
-                <h3 class="card__title">"${movie.Title}"</h3>
-                <p class="card__year">"${movie.Year}"</p>
-                <p class="card__rate">Рейтинг: "${movie.Type}"</p>
-              </div>
-        `;
-
+          if (data.Response === 'True') {
+            searchResults.forEach((movie) => {
+              const movieHTML = `
+          <div class="card">
+                  <img src="${movie.Poster}" alt="Cover" class="card__img" />
+                  <h3 class="card__title">"${movie.Title}"</h3>
+                  <p class="card__year">"${movie.Year}"</p>
+                  <p class="card__rate">Рейтинг: "${movie.Type}"</p>
+                </div>
+          `;
+              
             movieListNode.insertAdjacentHTML('beforeend', movieHTML);
           });
         }
 
     });
-  }
-  clearInput();
-  if (!movieName) {
+  }  
+  else {
     alert('Необходимо ввести название фильма');
     return;
   }
+  clearInput();
 });
