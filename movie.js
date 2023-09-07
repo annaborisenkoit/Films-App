b5b367802c2bee8dc659cfa135d38515fe13bd47
Anna Borisenko <anik4366@mail.ru>
2023-09-07 10:31:04 +0700

    1

--- movie.js
+++ movie.js
@@ -20,17 +20,17 @@ const renderMovie = (card) => {
           </div>
           <div class="card__list">
               <p class="card__title">${card.Title}</p>
-              <p class="card__year">Year: <span>${card.Year}</span></p>
-              <p class="card__rate">Rated:<span>${card.Rated}</span></p>
-              <p class="card__title">Released:<span>${card.Released}</span></p>
-              <p class="card__year">Runtime:<span>${card.Runtime}</span></p>
-              <p class="card__rate">Genre:<span>${card.Genre}</span></p>
-              <p class="card__title">Director:<span>${card.Director}</span></p>
-              <p class="card__year">Writer:<span>${card.Writer}</span></p>
-              <p class="card__rate">Actors:<span>${card.Actors}</span></p>
+              <p class="card__year">Year: ${card.Year}</p>
+              <p class="card__rate">Rated: ${card.Rated}</p>
+              <p class="card__title">Released: ${card.Released}</p>
+              <p class="card__year">Runtime: ${card.Runtime}</p>
+              <p class="card__rate">Genre: ${card.Genre}</p>
+              <p class="card__title">Director: ${card.Director}</p>
+              <p class="card__year">Writer: ${card.Writer}</p>
+              <p class="card__rate">Actors: ${card.Actors}</p>
           </div>
       </div>
-      <p class="movie__plot">${card.Plot}</p>
+      <p class="card__title">${card.Plot}</p>
     `;
   };
 
